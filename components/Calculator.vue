85a10f7179631bf15f3866c4c4401a59d6f4f08b
Adriaan van Rossum <1079135+adriaanvanrossum@users.noreply.github.com>
2022-12-26 23:31:10 +0100

    Add calculate logic

--- components/Calculator.vue
+++ components/Calculator.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
-    <p>Calculate a cross-multiplication without picking up a pen.</p>
+    <p>
+      Calculate a
+      <a href="https://en.wikipedia.org/wiki/Cross-multiplication"
+        >cross-multiplication</a
+      >
+      without picking up a pen.
+    </p>
 
-    <div class="max-w-xl mx-auto grid grid-cols-4 gap-4 justify-center mt-8">
+    <div
+      class="max-w-xl mx-auto grid grid-cols-4 gap-4 justify-center mt-8 mb-8"
+    >
       <div class="flex items-center justify-center">
         <input
           v-if="showUnitOne"
@@ -32,23 +40,38 @@
           v-for="(cell, cellIndex) in row"
           :key="`cell:${rowIndex}-${cellIndex}`"
         >
-          <div>
+          <div class="relative">
             <input type="text" v-model="matrix[rowIndex][cellIndex]" />
+            <a
+              @click="
+                matrix[rowIndex][cellIndex] = calculate(rowIndex, cellIndex)
+              "
+              v-if="calculate(rowIndex, cellIndex)"
+              class="absolute top-2 right-2 sm:p-1 border-2 border-violet-500 rounded cursor-pointer"
+            >
+              <Bars2Icon class="fill-violet-600 w-6 h-6 sm:w-7 sm:h-7" />
+            </a>
           </div>
         </template>
       </template>
-      <div class="col-span-2 flex justify-center items-center space-x-4">
+      <div class="mt-6 col-span-2 flex justify-center items-center space-x-4">
         <button
-          @click="addRow()"
+          @click="reset()"
           class="border-2 border-indigo-600 dark:border-indigo-400 rounded-xl hover:bg-indigo-600 dark:hover:bg-indigo-400"
         >
-          <PlusIcon class="w-12 h-12 p-2 fill-white" />
+          <TrashIcon class="w-12 h-12 p-2 stroke-white" />
         </button>
         <button
           @click="removeRow()"
           class="border-2 border-indigo-600 dark:border-indigo-400 rounded-xl hover:bg-indigo-600 dark:hover:bg-indigo-400"
         >
-          <MinusIcon class="w-12 h-12 p-2 fill-white" />
+          <MinusIcon class="w-12 h-12 p-2 stroke-white" />
+        </button>
+        <button
+          @click="addRow()"
+          class="border-2 border-indigo-600 dark:border-indigo-400 rounded-xl hover:bg-indigo-600 dark:hover:bg-indigo-400"
+        >
+          <PlusIcon class="w-12 h-12 p-2 stroke-white" />
         </button>
       </div>
     </div>
@@ -56,12 +79,19 @@
 </template>
 
 <script setup>
-import { PlusIcon, MinusIcon, PencilIcon } from "@heroicons/vue/24/solid";
+import {
+  PlusIcon,
+  MinusIcon,
+  PencilIcon,
+  Bars2Icon,
+  TrashIcon,
+} from "@heroicons/vue/24/outline";
 
 const unitOne = ref("People");
 const unitTwo = ref("Apples");
 const showUnitOne = ref(false);
 const showUnitTwo = ref(false);
+const resetDate = ref(new Date());
 
 const matrix = ref([
   [4, 12],
@@ -69,10 +99,51 @@ const matrix = ref([
 ]);
 
 const addRow = () => {
-  matrix.value.push([0, 0]);
+  matrix.value.push([0, null]);
 };
 const removeRow = () => {
   matrix.value.pop();
+};
+
+const reset = () => {
+  matrix.value = [
+    [4, 12],
+    [3, null],
+  ];
+  unitOne.value = "People";
+  unitTwo.value = "Apples";
+  resetDate.value = new Date();
+};
+
+// Do Cross-multiplication with the given row and column
+const calculate = (rowIndex, cellIndex) => {
+  // d = (b * c) / a
+  const isLeft = cellIndex === 0;
+
+  const row = matrix.value[rowIndex];
+  const b = row[isLeft ? 1 : 0];
+
+  if (!b) return;
+
+  const firstRow = matrix.value
+    .filter((_, index) => index !== rowIndex)
+    .find((row) => row[0] && row[1]);
+
+  if (!firstRow) return;
+
+  const [a, c] = firstRow;
+
+  const d = isLeft ? (b * a) / c : (b * c) / a;
+  const value = d.toFixed(3).replace(/\.?0+$/, "");
+
+  // only update cell when resetDate is newer than 200ms
+  const isNull = matrix.value[rowIndex][cellIndex] === null;
+  const isNotRecent = resetDate.value.getTime() + 200 < Date.now();
+  if (isNull && isNotRecent) {
+    matrix.value[rowIndex][cellIndex] = value;
+  }
+
+  return value;
 };
 </script>
 
